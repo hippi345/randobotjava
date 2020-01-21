6762f375fac270897fbcc3e6a764fa12bd47ba51
Matt Smith <matthew.j.smith2@outlook.com>
2020-01-21 08:21:26 -0800

    Adding IGame and changes hard references to the interface (#20)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,11 +3,13 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.interfaces.IGame;
+import sample.models.GameStatusEnum;
 
 // needs an extensive review before moving forward
 public class Main extends Application
 {
-    private static Game game;
+    private static IGame game;
     static int gridSizeForGame;
     static View gameView;
 
@@ -32,29 +34,28 @@ public class Main extends Application
 
         // setting up the buttons which go into the UI
         gameView.setupButtons(
-                (o) -> game.executeAutoMove(),
+                (o) -> game.MakeMove(),
                 (o) -> prepareGame(),
                 (o) -> RunAutoPlay(),
                 (o) -> backToStartup());
 
         gameView.setupDirectionButtons(game, gameView);
-        gameView.adjustBotAndTreasureLocations(game.bot, game.treasure);
+        gameView.adjustBotAndTreasureLocations(game.getBot(), game.getTreasure());
     }
 
     // run moveBot continually with the warning on infinite loops suppressed
     @SuppressWarnings("InfiniteLoopStatement")
     private static void RunAutoPlay()
     {
-        while(true)
+        while(game.GetStatus() != GameStatusEnum.Complete)
         {
-            game.executeAutoMove();
+            game.MakeMove();
         }
     }
 
     // sets up the Game object with bot and treasure objects in place with coordinates on the grid
     static void prepareGame() {
-        game = new Game(gridSizeForGame);
-        game.InitializeGame(gameView);
+        game = new Game(gridSizeForGame, gameView);
     }
 
     // method to return the GUI to the main menu
